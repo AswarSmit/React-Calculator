6dd11610e228f7a520fbc35d42c8f4f4cd86b5a6
Smit Aswar <smitaswar2000@gmail.com>
2023-10-06 16:23:32 +0530

    cahnged

--- src/App.js
+++ src/App.js
@@ -1,56 +1,82 @@
-
-import { useState } from 'react';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 
 function App() {
- 
-  const [result, setresult] = useState("")
-  const clickHandler=(event)=>{
-    setresult(result.concat(event.target.value))
-  }
+  const [result, setresult] = useState("");
+  const clickHandler = (event) => {
+    setresult(result.concat(event.target.value));
+  };
 
-  const clearDisplay=()=>{
-    setresult("")
-  }
+  const clearDisplay = () => {
+    setresult("");
+  };
 
-  const calculate=()=>{
-    setresult(eval(result).toString())
-    console.log(result)
-  }
-
+  const calculate = () => {
+    setresult(eval(result).toString());
+    console.log(result);
+  };
 
   return (
-    <div className='calculator-grid'>
-      <div className='output span-4' >
-       <div className='previous-oprand'></div>
-       <div className='current-oprand'>{result}</div>
-       
+    <div className="calculator-grid">
+      <div className="output span-4">
+        <div className="previous-oprand"></div>
+        <div className="current-oprand">{result}</div>
       </div>
-      <button className='span-three' onClick={clearDisplay}>AC</button>
-      
-      <button value="/" onClick={clickHandler}>/</button>   
-      <button value="1" onClick={clickHandler}>1</button> 
-      <button value="2" onClick={clickHandler}>2</button> 
-      <button value="3" onClick={clickHandler}>3</button> 
-      <button value="*" onClick={clickHandler}>*</button> 
-      <button value="4" onClick={clickHandler}>4</button> 
-      <button value="5"onClick={clickHandler}> 5</button> 
-      <button value="6" onClick={clickHandler}>6</button> 
-      <button value="+" onClick={clickHandler}>+</button> 
-      <button value="7" onClick={clickHandler}>7</button> 
-      <button value="8" onClick={clickHandler}>8</button>
-      <button value="9" onClick={clickHandler}>9</button> 
-      <button value="-" onClick={clickHandler}>-</button> 
-      <button value="." onClick={clickHandler}>.</button>
-      <button value="0" onClick={clickHandler}>0</button> 
-      <button className='span-two' onClick={calculate}>=</button>   
-    </div>
-    
+      <button className="span-three" onClick={clearDisplay}>
+        AC
+      </button>
 
-  )
-   
-  
+      <button value="/" onClick={clickHandler}>
+        /
+      </button>
+      <button value="1" onClick={clickHandler}>
+        1
+      </button>
+      <button value="2" onClick={clickHandler}>
+        2
+      </button>
+      <button value="3" onClick={clickHandler}>
+        3
+      </button>
+      <button value="*" onClick={clickHandler}>
+        *
+      </button>
+      <button value="4" onClick={clickHandler}>
+        4
+      </button>
+      <button value="5" onClick={clickHandler}>
+        
+        5
+      </button>
+      <button value="6" onClick={clickHandler}>
+        6
+      </button>
+      <button value="+" onClick={clickHandler}>
+        +
+      </button>
+      <button value="7" onClick={clickHandler}>
+        7
+      </button>
+      <button value="8" onClick={clickHandler}>
+        8
+      </button>
+      <button value="9" onClick={clickHandler}>
+        9
+      </button>
+      <button value="-" onClick={clickHandler}>
+        -
+      </button>
+      <button value="." onClick={clickHandler}>
+        .
+      </button>
+      <button value="0" onClick={clickHandler}>
+        0
+      </button>
+      <button className="span-two" onClick={calculate}>
+        =
+      </button>
+    </div>
+  );
 }
 
 export default App;
